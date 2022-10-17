281b0bef32fd4db501247bb5561973b9f216f0d1
Tanujarora100 <108284546+Tanujarora100@users.noreply.github.com>
2022-10-17 11:46:25 +0530

    Update topologicalSort.java

--- Graphs/topologicalSort.java
+++ Graphs/topologicalSort.java
@@ -1,36 +1,51 @@
-class Solution
-{
-    //Function to return list containing vertices in Topological order. 
-    static void dfs(int start, boolean[] visited, Stack<Integer> st, ArrayList<ArrayList<Integer>> adj)
-    {
-        visited[start]=true;
-        for(int neighbour:adj.get(start))
-        {
-            if(visited[neighbour]==false)
-            {
-                dfs(neighbour,visited,st,adj);
+package Graphs.Algorithm;
+
+import java.util.*;
+
+public class topologicalSort {
+    static void addEdge(ArrayList<ArrayList<Integer>> adj, int source, int dest) {
+        adj.get(source).add(dest);
+    }
+
+    static void dfs(int start, ArrayList<ArrayList<Integer>> adj, boolean[] visited, Stack<Integer> st) {
+        visited[start] = true;
+        for (int neighbour : adj.get(start)) {
+            if (!visited[neighbour]) {
+                dfs(neighbour, adj, visited, st);
             }
         }
-        st.push(start);
-        
+        st.add(start);
     }
-    
-    
-    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
-    {
-        boolean visited[]= new boolean[V];
-        int ans[]= new int[V];
-        Stack<Integer> st= new Stack<>();
-        for(int i=0;i<V;i++)
-        {
-            if(visited[i]==false) { dfs(i,visited,st,adj); }
+
+    static void topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean visited[] = new boolean[V];
+        int arr[] = new int[V];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, st);
+            }
         }
-        int i=0;
-        while(!st.isEmpty())
-        {  
-            ans[i++]=st.pop();
-            
-        }         
-        return ans;
+
+        while (!st.isEmpty()) {
+            int temp = st.pop();
+            System.out.print(temp + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        int V = 5;
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<Integer>());
+
+        addEdge(adj, 0, 1);
+        addEdge(adj, 1, 3);
+        addEdge(adj, 2, 3);
+        addEdge(adj, 3, 4);
+        addEdge(adj, 2, 4);
+
+        System.out.println("Following is a Topological Sort of:");
+        topoSort(V, adj);
     }
 }
